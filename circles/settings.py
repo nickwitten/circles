3701b365162c35d6c5136750a756403f6a006dcc
Nick Witten <nwitt12@gmail.com>
2021-12-27 00:33:28 -0500

    Secret key from environment

--- circles/settings.py
+++ circles/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j96da(gg2-eev8lo0q$oi89om!$5&_yfg&6ke9=$xhog2lbr6g'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
