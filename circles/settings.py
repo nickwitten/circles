2e014f236d02295b92c7394e5a9d4867e0150119
Nick Witten <nwitt12@gmail.com>
2024-01-19 07:58:29 -0500

    settings for environment variables

--- circles/settings.py
+++ circles/settings.py
@@ -20,13 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+if 'DJANGO_SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+else:
+    from django.core.management.utils import get_random_secret_key
+    print("Generating Django secret key. Please set environment variable 'DJANGO_SECRET_KEY' in production.")
+    SECRET_KEY = get_random_secret_key()
+
 
 # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.us-east-1.amazonaws.com'
-if "RUN_LOCAL" in os.environ:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    MEDIA_URL = '/media/'
-else:
+if 'AWS_ACCESS_KEY_ID' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_S3_OBJECT_PARAMETERS = {
@@ -36,10 +39,14 @@ else:
     DEFAULT_FILE_STORAGE = 'circles.storage_backends.MediaStorage'
     MEDIA_ROOT = '/'
     MEDIA_URL = 'http://s3.amazonaws.com/circles-mediastorage/'
-
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
+    print(f"Setting media storage to local directory: {MEDIA_ROOT}")
+    print("Please make sure this is desired in production.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if not DEBUG:
     ALLOWED_HOSTS = ['circles-env.eba-nzmkvdv4.us-east-1.elasticbeanstalk.com']
